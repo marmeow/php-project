3b8a6492af9cb37a073bddc8e1c30d70eac04d46
marramiau123 <marfernandezvidal@gmail.com>
2025-11-13 00:44:14 +0100

    Gnerar productes ver1
    Falta oragnizar bien el codigo (en funciones en lugar de todo suelto) y poner imagenes en el json

--- public/script.js
+++ public/script.js
@@ -1,20 +1,3 @@
-//https://www.w3schools.com/howto/howto_js_vertical_tabs.asp
-
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-
 // https://codepen.io/webstuff/pen/JKgwZY// 
 
 // Add to cart 
@@ -67,21 +50,145 @@ document.addEventListener('click', function (e) {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  fetch('products/products.json')
+  fetch('../products/products.json')
     .then(response => response.json())
     .then(datos => {
-      document.getElementById("nombre").textContent = datos.nom;
-      document.querySelector(".preu").textContent = datos.preu;
-      document.querySelector(".desc").textContent = datos.ciudad;
+      // para agrupar els productes en categpories
+      const productesPorCategoria = {
+        'entrepans-freds': [],
+        'entrepans-calents': [],
+        'brioixeria': [],
+        'snacks': [],
+        'begudes': [],
+        'fruita': []
+      };
 
-      const lista = document.getElementById("hobbies");
-      datos.hobbies.forEach(hobby => {
-        const li = document.createElement("li");
-        li.textContent = hobby;
-        lista.appendChild(li);
+      // classificar els productes 
+      datos.productes.forEach(producte => {
+        if (producte.categoria === 'entrepans') {
+          // Segons si s'inclou la praula fred o calent a la descripcio els entrepans van a fred o calent
+          if (producte.descripció && producte.descripció.toLowerCase().includes('fred')) {
+            productesPorCategoria['entrepans-freds'].push(producte);
+          } else {
+            productesPorCategoria['entrepans-calents'].push(producte);
+          }
+        } else {
+          productesPorCategoria[producte.categoria]?.push(producte);
+        }
       });
-    })
-    .catch(error => console.error("Error al cargar el JSON:", error));
 
+
+
+      // FALTA HACER LO DEL DOM PURIFY!!!!!!!!!!!!! AÑADIR AL JIRA MAÑANA!!!!!!!!!!!!!!!!!!!!!!! 
+
+      function crearTarjetaProducte(producte) {
+        const ofertaClass = producte.oferta ? 'oferta' : '';
+        const imatgeUrl = producte.imatge || 'placeholder.png';
+
+        return `
+      <article class="product-card ${ofertaClass}" data-producte="${producte.nom}">
+        <img src="../resources/images/${imatgeUrl}" alt="${producte.nom}">
+        <h5>${producte.nom}</h5>
+        <p class="descripcio">${producte.descripció || producte.descripcio}</p>
+        <div class="preu-oferta-container">
+          <p class="preu-iva"><span class="preu">${producte.preu.toFixed(2)}</span> € <span class="iva">IVA inclós</span></p>
+          <div class="oferta-badge">
+            ${producte.oferta ? 'Oferta!' : ''}
+          </div>
+        </div>
+        <div class="cont">
+          <div class="crtdiv">
+            <button type="button" class="cart btn-cart">
+              Afegir a la cistella
+            </button>
+            <span class="qty">
+              <span class="dec">
+                <i class="fa fa-minus" aria-hidden="true"></i>
+              </span>
+              <span class="num">1</span>
+              <span class="inc">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+              </span>
+            </span>
+          </div>
+        </div>
+      </article>
+    `;
+      }
+
+      
+      
+      // GENERAR!!!!!!!!!!!!!!! TODO ESTO LO METERÉ EN UNA FUNCION EN LUGAR DE DEJARLO SUELTO!!!!!!!!!!!!!!!!!! AÑADIRE TASCA AL JIRA MAÑANA!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+      const fredsContainer = document.querySelector('#freds .product-cards');
+      if (fredsContainer && productesPorCategoria['entrepans-freds'].length > 0) {
+        fredsContainer.innerHTML = productesPorCategoria['entrepans-freds']
+          .map(p => crearTarjetaProducte(p))
+          .join('');
+      }
+
+
+      const calentsContainer = document.querySelector('#calents');
+      if (calentsContainer && productesPorCategoria['entrepans-calents'].length > 0) {
+        let calentsCards = calentsContainer.querySelector('.product-cards');
+        if (!calentsCards) {
+          calentsCards = document.createElement('div');
+          calentsCards.className = 'product-cards flex';
+          calentsContainer.appendChild(calentsCards);
+        }
+        calentsCards.innerHTML = productesPorCategoria['entrepans-calents']
+          .map(p => crearTarjetaProducte(p))
+          .join('');
+      }
+
+      const brioixeriaContainer = document.querySelector('#brioixeria .product-cards');
+      if (brioixeriaContainer && productesPorCategoria['brioixeria'].length > 0) {
+        brioixeriaContainer.innerHTML = productesPorCategoria['brioixeria']
+          .map(p => crearTarjetaProducte(p))
+          .join('');
+      }
+
+
+      const snacksContainer = document.querySelector('#snacks .product-cards');
+      if (snacksContainer && productesPorCategoria['snacks'].length > 0) {
+        snacksContainer.innerHTML = productesPorCategoria['snacks']
+          .map(p => crearTarjetaProducte(p))
+          .join('');
+      }
+
+      const begudesContainer = document.querySelector('#begudes .product-cards');
+      if (begudesContainer && productesPorCategoria['begudes'].length > 0) {
+        begudesContainer.innerHTML = productesPorCategoria['begudes']
+          .map(p => crearTarjetaProducte(p))
+          .join('');
+      }
+
+      const fruitaContainer = document.querySelector('#fruita .product-cards');
+      if (fruitaContainer && productesPorCategoria['fruita'].length > 0) {
+        fruitaContainer.innerHTML = productesPorCategoria['fruita']
+          .map(p => crearTarjetaProducte(p))
+          .join('');
+      }
+    })
+    .catch(error => console.error("Error al carregar el JSON:", error));
 });
 
+
+//https://www.w3schools.com/howto/howto_js_vertical_tabs.asp
+
+function openCity(evt, cityName) {
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
