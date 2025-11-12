4b858f4a4b39785ec8185fde6559645031bee56c
Alex-Potlog <alexpotloghary@gmail.com>
2025-11-12 18:44:37 +0100

    Remogut la funció en favor de ser trucat de forma directa

--- php/confirm.php
+++ php/confirm.php
@@ -1,27 +1,23 @@
 <?php
-
-
-function rebAjaxRequest()
-{
-    $data = json_decode(file_get_contents("php://input"), true);
-    $nom = $data['nom'];
-    $nom = filter_var($nom, FILTER_SANITIZE_STRING);
-    $data['nom'] = $nom;
-    $email = $data['email'];
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    $data['email'] = $email;
-    $nomArxiu = "../tiquets/tiquet-" . $nom . ".txt";
-    $aux = 0;
-    while (file_exists($nomArxiu)) {
-        $aux++;
-        $nomArxiu = "../tiquets/tiquet-" . $nom . "(" . $aux . ").txt";
-    }
-
-    if (!file_exists($nomArxiu)) {
-        touch($nomArxiu);
-        $arxiu = fopen($nomArxiu, "a");
-        fwrite($arxiu, json_encode($data));
-        fclose($arxiu);
-    }
+$data = json_decode(file_get_contents("php://input"), true);
+$nom = $data['nom'];
+$nom = filter_var($nom, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$data['nom'] = $nom;
+$email = $data['email'];
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+$data['email'] = $email;
+$nomArxiu = "../tiquets/tiquet-" . $nom . ".txt";
+$aux = 0;
+while (file_exists($nomArxiu)) {
+    $aux++;
+    $nomArxiu = "../tiquets/tiquet-" . $nom . "(" . $aux . ").txt";
 }
+
+if (!file_exists($nomArxiu)) {
+    touch($nomArxiu);
+    $arxiu = fopen($nomArxiu, "a");
+    fwrite($arxiu, json_encode($data));
+    fclose($arxiu);
+}
+
 
