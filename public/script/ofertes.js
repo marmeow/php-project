9a4c0d474879f6d5a2066224680bd8c0c88537f4
Marta-daw <marta.llab.daw@gmail.com>
2025-11-14 10:46:06 +0100

    aplicar estils

--- public/script/ofertes.js
+++ public/script/ofertes.js
@@ -50,13 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 articleOfertes.appendChild(pPreuAbans);
                 articleOfertes.appendChild(pPreuDescompte);
                 ofert.appendChild(articleOfertes);
-                return `
-                    <article class="ofertProduct"> 
-                        <h3> ${producte.nom} </h3>
-                        <p> ${producte.descripció || producte.descripcio} </p>
-                        <p class="preu-antiga"> Preu Antic: ${(producte.preu).toFixed(2)} € </p>
-                        <p class="preu-nou"> Preu Oferta: ${(producte.preu * 0.8).toFixed(2)} € </p>
-                    </article>`;
             }
 
             // Renderitzar les cartes de productes amb oferta
@@ -65,9 +58,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 ofertContainer.append(productesCategories['ofert']
                     .map(p => crearTarjetaProducte(p))
                     .join(''));
-                /* ofertContainer.innerHTML = productesCategories['ofert']
-                    .map(p => crearTarjetaProducte(p))
-                    .join(''); */
             }
         })
         .catch(error => console.error("Error al carregar el JSON:", error));
