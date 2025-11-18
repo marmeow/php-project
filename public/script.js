73b8f01c3cfb842ff437a0089e98f8386bbb7641
mar <marfernandezvidal@gmail.com>
2025-11-18 13:03:41 +0100

    Modificado script añadir productos

--- public/script.js
+++ public/script.js
@@ -52,143 +52,80 @@ document.addEventListener('click', function (e) {
 
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
   fetch('../data/products.json')
     .then(response => response.json())
-    .then(datos => {
-      // para agrupar els productes en categpories
-      const productesPorCategoria = {
-        'entrepans-freds': [],
-        'entrepans-calents': [],
-        'brioixeria': [],
-        'snacks': [],
-        'begudes': [],
-        'fruita': []
-      };
-
-      // classificar els productes 
-      datos.productes.forEach(producte => {
-        if (producte.categoria === 'entrepans') {
-          // Segons si s'inclou la praula fred o calent a la descripcio els entrepans van a fred o calent
-          if (producte.descripció && producte.descripció.toLowerCase().includes('fred')) {
-            productesPorCategoria['entrepans-freds'].push(producte);
-          } else {
-            productesPorCategoria['entrepans-calents'].push(producte);
-          }
-        } else {
-          productesPorCategoria[producte.categoria]?.push(producte);
-        }
-      });
-
-
-
-      // FALTA HACER LO DEL DOM PURIFY!!!!!!!!!!!!! AÑADIR AL JIRA MAÑANA!!!!!!!!!!!!!!!!!!!!!!! 
-
-      function crearTarjetaProducte(producte) {
-        const ofertaClass = producte.oferta ? 'oferta' : '';
-        const imatgeUrl = producte.imatge || "placeholder.png";
-        console.log(producte.imatge)
-        let content = `
-      <article class="product-card ${ofertaClass}" data-producte="${producte.nom}">
-        <div class="inner">
-
-        <img src="../resources/images/products/${imatgeUrl}" alt="${producte.nom}">
-          <h5>${producte.nom}</h5>
-          <p class="descripcio">${producte.descripció || producte.descripcio}</p>
-          <div class="preu-oferta-container">
-            <p class="preu-iva"><span class="preu">${producte.preu.toFixed(2)}</span> € <span class="iva">IVA inclós</span></p>
-            <div class="oferta-badge">
-              ${producte.oferta ? 'Oferta!' : ''}
-            </div>
-          </div>
-          <div class="cont">
-            <div class="crtdiv">
-              <button type="button" class="cart btn-cart">
-                Afegir a la cistella
-              </button>
-              <span class="qty">
-                <span class="dec">
-                  <i class="fa fa-minus" aria-hidden="true"></i>
-                </span>
-                <span class="num">1</span>
-                <span class="inc">
-                  <i class="fa fa-plus" aria-hidden="true"></i>
-                </span>
-              </span>
-            </div>
-          </div>
-
-        </div>
-
-
-        
-
-      </article>
-    `;
-
-
-        return content;
-      }
-
-
-
-      // GENERAR!!!!!!!!!!!!!!! TODO ESTO LO METERÉ EN UNA FUNCION EN LUGAR DE DEJARLO SUELTO!!!!!!!!!!!!!!!!!! AÑADIRE TASCA AL JIRA MAÑANA!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-      const fredsContainer = document.querySelector('#freds .product-cards');
-      if (fredsContainer && productesPorCategoria['entrepans-freds'].length > 0) {
-        fredsContainer.innerHTML = productesPorCategoria['entrepans-freds']
-          .map(p => crearTarjetaProducte(p))
-          .join('');
-      }
-
-
-      const calentsContainer = document.querySelector('#calents');
-      if (calentsContainer && productesPorCategoria['entrepans-calents'].length > 0) {
-        let calentsCards = calentsContainer.querySelector('.product-cards');
-        if (!calentsCards) {
-          calentsCards = document.createElement('div');
-          calentsCards.className = 'product-cards flex';
-          calentsContainer.appendChild(calentsCards);
-        }
-        calentsCards.innerHTML = productesPorCategoria['entrepans-calents']
-          .map(p => crearTarjetaProducte(p))
-          .join('');
-      }
-
-      const brioixeriaContainer = document.querySelector('#brioixeria .product-cards');
-      if (brioixeriaContainer && productesPorCategoria['brioixeria'].length > 0) {
-        brioixeriaContainer.innerHTML = productesPorCategoria['brioixeria']
-          .map(p => crearTarjetaProducte(p))
-          .join('');
-      }
-
-
-      const snacksContainer = document.querySelector('#snacks .product-cards');
-      if (snacksContainer && productesPorCategoria['snacks'].length > 0) {
-        snacksContainer.innerHTML = productesPorCategoria['snacks']
-          .map(p => crearTarjetaProducte(p))
-          .join('');
-      }
-
-      const begudesContainer = document.querySelector('#begudes .product-cards');
-      if (begudesContainer && productesPorCategoria['begudes'].length > 0) {
-        begudesContainer.innerHTML = productesPorCategoria['begudes']
-          .map(p => crearTarjetaProducte(p))
-          .join('');
-      }
-
-      const fruitaContainer = document.querySelector('#fruita .product-cards');
-      if (fruitaContainer && productesPorCategoria['fruita'].length > 0) {
-        fruitaContainer.innerHTML = productesPorCategoria['fruita']
-          .map(p => crearTarjetaProducte(p))
-          .join('');
-      }
+    .then(dades => {
+      const productesPerCategoria = agruparItems(dades.productes);
+      mostrarCategories(productesPerCategoria);
     })
     .catch(error => console.error("Error al carregar el JSON:", error));
 });
+
+function agruparItems(productes) {
+  const cats = {};
+  productes.forEach(item => {
+    if (item.categoria === 'entrepans') {
+      const subcat = (item.descripció?.toLowerCase().includes('fred'))
+        ? 'entrepans-freds'
+        : 'entrepans-calents';
+      if (!cats[subcat]) cats[subcat] = [];
+      cats[subcat].push(item);
+    } else {
+      if (!cats[item.categoria]) cats[item.categoria] = [];
+      cats[item.categoria].push(item);
+    }
+  });
+  return cats;
+}
+
+function crearCard(item) {
+  const ofertaClass = item.oferta ? 'oferta' : '';
+  const imgUrl = item.imatge || "placeholder.png";
+  return `
+    <article class="product-card ${ofertaClass}" data-producte="${item.nom}">
+      <div class="inner">
+        <img src="../resources/images/products/${imgUrl}" alt="${item.nom}">
+        <h5>${item.nom}</h5>
+        <p class="descripcio">${item.descripció || item.descripcio}</p>
+        <p class="preu">${item.preu.toFixed(2)} €</p>
+      </div>
+    </article>
+  `;
+}
+
+function mostrarCategories(categories) {
+  const productesDiv = document.getElementById("productes");
+
+  Object.keys(categories).forEach(cat => {
+    const items = categories[cat];
+    if (items.length === 0) return;
+
+    if (cat === "entrepans-freds") {
+      const fredsCards = document.querySelector("#freds .product-cards");
+      if (fredsCards) {
+        fredsCards.innerHTML = items.map(crearCard).join("");
+      }
+    } else if (cat === "entrepans-calents") {
+      const calentsCards = document.querySelector("#calents .product-cards");
+      if (calentsCards) {
+        calentsCards.innerHTML = items.map(crearCard).join("");
+      }
+    } else {
+      let sec = document.getElementById(cat);
+      if (!sec) {
+        sec = document.createElement("section");
+        sec.id = cat;
+        const titulo = cat.charAt(0).toUpperCase() + cat.slice(1);
+        sec.innerHTML = `<h3>${titulo}</h3><div class="product-cards flex"></div>`;
+        productesDiv.appendChild(sec);
+      }
+      const cards = sec.querySelector(".product-cards");
+      cards.innerHTML = items.map(crearCard).join("");
+    }
+  });
+}
+
 
 
 //https://www.w3schools.com/howto/howto_js_vertical_tabs.asp
