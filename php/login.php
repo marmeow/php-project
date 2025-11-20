612861588c471791686f1c79faaa5ded28aed69b
Marta-daw <marta.llab.daw@gmail.com>
2025-11-20 18:42:29 +0100

    temp

--- php/login.php
+++ php/login.php
@@ -17,15 +17,30 @@ $usuarios = json_decode($json, true);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $resposta=["correcte"=>false];
     // Recollir i sanejar les dades
-    $nombre = trim($_POST['userName'] ?? '');
-    $password = trim($_POST['password'] ?? '');
+    $nombre = trim($data['userName'] ?? '');
+    $password = trim($data['password'] ?? '');
 
-    if(isset($usuarios[$nombre]) && $password == $usuarios[$nombre]["contrasenya"]){
-        session_regenerate_id(true); //Quan fas el login et genera una altra sessió internament
-        $_SESSION['usuari'] = $usuarios[$nombre];
-        $_SESSION['admin'] = $usuarios[$nombre]["admin"];
-        header("Location: ../public/index.html");
-        exit();
+    $nombre=filter_var($nombre, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $password=filter_var($password, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    // Bucle per a recorrer l'array del document json
+    $trobat = array_filter($usuarios["usuaris"], function($user) use ($nombre) {
+        return $user["nom"] === $nombre;
+    });
+
+    // Si s'ha trobat el nom de l'usuari indicat
+    if(!empty($trobat)){
+        $usuariTrobat=reset($trobat); //agafa el primer element d'una array
+        if($password == $usuariTrobat["contrasenya"]){
+            session_regenerate_id(true); //Quan fas el login et genera una altra sessió internament
+            $_SESSION['usuari'] = $usuariTrobat["nom"];
+            $_SESSION['admin'] = $usuariTrobat["admin"];
+            $resposta["correcte"]=true;
+            echo json_encode($resposta);
+            exit();
+        } else{
+            echo json_encode($resposta);
+        }
     } else{
         echo json_encode($resposta);
     }
